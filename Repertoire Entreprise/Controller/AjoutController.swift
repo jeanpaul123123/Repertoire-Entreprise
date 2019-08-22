fbdc109662ae349053c08e5a0d50e1ba96df4d60
Jean-Paul <jeanpaul123@gmail.com>
2019-08-22 21:25:39 +0200

    Chargement des entreprises

--- Repertoire Entreprise/Controller/AjoutController.swift
+++ Repertoire Entreprise/Controller/AjoutController.swift
@@ -51,21 +51,51 @@ class AjoutController: UIViewController {
     //#fetchrequete
     func fetchEntreprises() {
         let requete: NSFetchRequest<Entreprise> = Entreprise.fetchRequest()
+        // tri des données
         let tri = NSSortDescriptor(key: "nom", ascending: true)
         requete.sortDescriptors = [tri]
+        // fin tri
         do {
+            // actualisation du tableau des entreprises qui alimente le pickerview (voir PickerViewExtension)
             entreprises = try contexte.fetch(requete)
+            // rafraichissement du picker
             pickerView.reloadAllComponents()
         } catch {
             print(error.localizedDescription)
         }
     }
     
+    
+    @IBAction func ajouterEntrepriseAction(_ sender: Any) {
+        // le UIAlertController est la popup
+        let alerte = UIAlertController(title: "Votre entreprise n'est pas dans la liste?", message: "Ajouter", preferredStyle: .alert)
+        // le UIAlertAction sont les boutons d'action
+        let ajout = UIAlertAction(title: "OK", style: .default) { (action) in
+            let textField = alerte.textFields![0] as UITextField
+            if let texte = textField.text, texte != "" {
+                // création d'une entreprise par le contexte ..
+                let nouvelleEntreprise = Entreprise(context: contexte)
+                nouvelleEntreprise.nom = texte
+                // Enregistrement des objets
+                appDelegate.saveContext()
+                self.fetchEntreprises()
+            }
+            
+        }
+        // texte en filigrane dans le champs de saisie
+        alerte.addTextField { (tf) in
+            tf.placeholder = "nom de l'entreprise"
+        }
+        let annuler = UIAlertAction(title: "Annuler", style: .default, handler: nil)
+        alerte.addAction(ajout)
+        alerte.addAction(annuler)
+        self.present(alerte, animated: true, completion: nil)
+    }
+    
     @IBAction func ajouterPersonneAction(_ sender: Any) {
     }
     
-    @IBAction func ajouterEntrepriseAction(_ sender: Any) {
-    }
+
     @IBOutlet weak var largeurContrainte: NSLayoutConstraint!
     
     @IBOutlet weak var contrainteDuBas: NSLayoutConstraint!
