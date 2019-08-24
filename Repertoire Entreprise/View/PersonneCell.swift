71361f0b2753ca9710fac475a1f107a6b1b414e7
Jean-Paul <jeanpaul123@gmail.com>
2019-08-24 13:07:23 +0200

    Création en cours de la tableview

---
+++ Repertoire Entreprise/View/PersonneCell.swift
@@ -0,0 +1,19 @@
+//
+//  PersonneCell.swift
+//  Repertoire Entreprise
+//
+//  Created by Jean-Paul Berthelot on 24/08/2019.
+//  Copyright © 2019 Jean-Paul Berthelot. All rights reserved.
+//
+
+import UIKit
+
+class PersonneCell: UITableViewCell {
+
+    
+    
+    
+
+
+
+}
